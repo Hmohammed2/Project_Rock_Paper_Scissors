de6cb1bb8b0cd79a8ff317e9f80d4a9b4731ce3f
Hmohammed2 <hamza_mohammed15@hotmail.com>
2022-04-27 02:34:14 +0100

    Added in function for getting the winner

--- manual_rps.py
+++ manual_rps.py
@@ -14,8 +14,25 @@ def get_user_choice():
     else:
         print("You can only pick between Rock, Paper, Scissors")
 
-def get_winner(user_choice, computer_choice):
 
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print(f"You picked {user_choice} and computer picked {computer_choice}. Its a tie!")
+    elif user_choice == "Rock":
+        if computer_choice == "Paper":
+            print("Too bad you lose! Paper beats rock!")
+        else:
+            print("Well done you win! Rock smashes scissors")
+    elif user_choice == "Paper":
+        if computer_choice == "Rock":
+            print("Well done you win! Paper beats rock!")
+        else:
+            print("Too bad you lose! Scissors cuts paper!")
+    elif user_choice == "Scissors":
+        if computer_choice == "Rock":
+            print("Too bad you lose! Rock smashes scissors!")
+        else:
+            print("Well done you win! Scissors cuts paper!")
 
 if __name__ == '__main__':
     get_user_choice()
