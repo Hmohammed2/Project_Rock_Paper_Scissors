a5cd047ac4213d2690e96501db7dfa0c09924a06
Hmohammed2 <hamza_mohammed15@hotmail.com>
2022-04-28 05:16:06 +0100

    Added in score system as well as message to play again

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from keras.models import load_model
 import numpy as np
 import random as rd
 
+player_score = 0
+computer_score = 0
+
+
 class Game:
     def __init__(self):
         # Constructor
@@ -23,34 +27,44 @@ class Game:
         return user_choice
 
     def get_winner(self, user_choice):
+        global player_score, computer_score
+
         computer_choice = self.get_computer_choice()
 
         print(f"Computer picks {computer_choice}")
 
         if user_choice == computer_choice:
             print(f"You picked {user_choice} and computer picked {computer_choice}. Its a tie!")
+            player_score += 1
+            computer_score += 1
         elif user_choice == "rock":
             if computer_choice == "paper":
                 print("Too bad you lose! Paper beats rock!")
+                computer_score += 1
             else:
                 print("Well done you win! Rock smashes scissors")
+                player_score += 1
         elif user_choice == "paper":
             if computer_choice == "rock":
                 print("Well done you win! Paper beats rock!")
+                player_score += 1
             else:
                 print("Too bad you lose! Scissors cuts paper!")
+                computer_score += 1
         elif user_choice == "scissors":
             if computer_choice == "rock":
                 print("Too bad you lose! Rock smashes scissors!")
+                computer_score += 1
             else:
                 print("Well done you win! Scissors cuts paper!")
+                player_score += 1
 
 
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 game = Game() # Initialise game class
-counter = 50
+counter = 70
 
 while True:
     ret, frame = cap.read()
@@ -83,17 +97,26 @@ while True:
     counter = counter - 1
 
     if counter == 0:
-        user_choice = prediction_output(max_value)
-        print(user_choice)
-        game.get_winner(user_choice)
-        counter = 50
+        player_choice = prediction_output(max_value)
+        print(player_choice)
+        game.get_winner(player_choice)
+        print(f"Player: {player_score}")
+        print(f"Computer: {computer_score}")
+        counter = 70
+
+    if player_score or computer_score = 3:
+        play_again = input("Play again (y/n)")
+        if play_again.lower() != "y":
+            break
 
     # Press q to close the window
     # print(prediction)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    #    play_again = input("Play again (y/n)")
+    #    if play_again.lower() != "y":
+    #        break
 
 # After the loop release the cap object
 cap.release()
