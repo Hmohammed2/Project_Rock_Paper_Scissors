d875a30cb9b58afca05ced695370b5a4bb58915e
Hmohammed2 <hamza_mohammed15@hotmail.com>
2022-04-29 19:13:08 +0100

    Sort out code into functions

--- camera_rps.py
+++ camera_rps.py
@@ -1,3 +1,5 @@
+import time
+
 from keras.models import load_model
 import numpy as np
 import random as rd
@@ -61,28 +63,32 @@ model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 game = Game()  # Initialise game class
-counter = 70
+countdown = 80  # Timer until game starts
 
 while True:
+
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
     image_np = np.array(resized_frame)
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1  # Normalize the image
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, str(counter),  # Puts in a countdown timer to let user know when to input hand gesture
+    cv2.putText(frame, str(countdown),  # Puts in a countdown timer to let user know when to input hand gesture
                 (200, 250), font,
                 7, (0, 255, 255),
                 4, cv2.LINE_AA)
     data[0] = normalized_image
     prediction = model.predict(data)
     cv2.imshow('frame', frame)
-    max_value = np.where(prediction == np.amax(prediction)) > 0.7
+    max_value = np.where(prediction == np.amax(prediction))
     # pulls out the max probability value and locates the index which it is located in. THe index represents the label
     # 0 label is rock, 1 label is scissors, 2 label is paper, 3 label is nothing
 
+    # countdown variable which counts to 0 and initiates the users most probable hand gesture
+    countdown = countdown - 1
+
 
     def prediction_output(max_probability):  # function gets the max probability value from the multidimensional array
-        # and returns the gesture
+        # and returns the label corresponding to the gesture
         if max_probability[1] == 0:
             users_choice = "rock"
         elif max_probability[1] == 1:
@@ -94,17 +100,19 @@ while True:
         return users_choice
 
 
-    # counter variable which counts to 0 and initiates the users most probable hand gesture
-    counter = counter - 1
+    def score_system():  # function stores the score
+        global player_score, computer_score
 
-    if counter == 0:
         player_choice = prediction_output(max_value)
         print(f"You picked {player_choice}")
         game.get_winner(player_choice)
         print(f"Player: {player_score}")
         print(f"Computer: {computer_score}")
-        counter = 70
 
+
+    if countdown == 0:
+        score_system()
+        countdown = 80  # restart the counter
     if player_score == 3 or computer_score == 3:
         if player_score == 3:
             print("Well done you beat the computer!")
@@ -118,7 +126,6 @@ while True:
             computer_score = 0
 
     # Press q to close the window
-    # print(prediction)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
